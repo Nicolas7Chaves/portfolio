e3b692c442cd4c4b2af4ff6e36c6260a8b8fa5c2
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-01-23 17:03:50 -0500

    add useState for skills and JSON for Database

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,12 +1,16 @@
 import "./Hero.scss";
 import Portrait from "../../Assets/headshot.jpg";
+import skillsData from "../../Data/skillsData.json"
+import { useState } from "react";
 
 function Hero() {
+    const [skills, setSkills] = useState(skillsData);
+
     return (
         <>
             <section className="home" id="home">
                 <section className="hero" >
-                <img className="hero__portrait" src={Portrait} alt="portrait of Nicolas"></img>
+                    <img className="hero__portrait" src={Portrait} alt="portrait of Nicolas"></img>
                     <h1 class="section__title section__title--hero">
                         Hey! I'm <strong className="section__title--name">Nicolas Chaves</strong>
                     </h1>
@@ -15,38 +19,16 @@ function Hero() {
             </section>
 
             <section className="skills">
-                <h2 className="section_title section__title--skills">
+                <h2 className="section__title section__title--skills">
                     Skills
                 </h2>
                 <div className="skills__container">
-                    <div className="skills__item">
-                        <img className="skills__image" />
-                        <p className="skills__title">test test test test</p>
-                    </div>
-                    <div className="skills__item">
-                        <img className="skills__image" />
-                        <p className="skills__title">test test test test</p>
-                    </div>
-                    <div className="skills__item">
-                        <img className="skills__image" />
-                        <p className="skills__title">test test test test</p>
-                    </div>
-                    <div className="skills__item">
-                        <img className="skills__image" />
-                        <p className="skills__title">test test test test</p>
-                    </div>
-                    <div className="skills__item">
-                        <img className="skills__image" />
-                        <p className="skills__title">test test test test</p>
-                    </div>
-                    <div className="skills__item">
-                        <img className="skills__image" />
-                        <p className="skills__title">test test test test</p>
-                    </div>
-                    <div className="skills__item">
-                        <img className="skills__image" />
-                        <p className="skills__title">test test test test</p>
-                    </div>
+                    {skills.map((skill, index) => (
+                        <div key={index} className="skills__item">
+                            <img className="skills__image" src={skill.img} alt={skill.title}/>
+                            <p className="skills__title">{skill.title}</p>
+                        </div>
+                    ))}
                 </div>
             </section>
         </>
