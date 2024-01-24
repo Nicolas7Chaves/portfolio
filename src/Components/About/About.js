2480c23d23d29030263e9727816defa6e604872e
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-01-24 14:40:57 -0500

    finalize v1 for about me

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,20 +1,24 @@
 import '../../Pages/MainPage/MainPage.scss';
 import "./About.scss";
 import aboutMe from "../../Assets/demoday.jpeg";
+import { TypeAnimation } from 'react-type-animation';
 
 function About() {
 
     return (
         <section className="about" id="about">
-                <div className='about__layout'>
-                    <h2 className="section__title section__title--about">Who I am</h2>
-                    {/* <p className="section__subtitle section__subtitle--about"> Software Engineer based out of Miami</p> */}
-
-                    <div className="about__body">
-                        I'm a recent graduate from BrainStation, where I cultivated a passion for Software Development with a strong background in Real Estate Analysis. I'm eager to continue my journey in Software Engineering, applying my creativity and problem-solving skills to develop innovative solutions.
-                    </div>
+            <div className='about__layout'>
+                <h2 className="section__title section__title--about">Who I am</h2>
+                <TypeAnimation
+                    sequence={['Software Engineer', 600, 'Analyst', 600, 'Crossfitter', 600]}
+                    repeat={Infinity}
+                    className="section__subtitle--about"
+                />
+                <div className="about__body">
+                    I'm a recent graduate from BrainStation, where I cultivated a passion for Software Development with a strong background in Real Estate Analysis. I'm eager to continue my journey in Software Engineering, applying my creativity and problem-solving skills to develop innovative solutions.
                 </div>
-                <img src={aboutMe} alt="" className='about__img'></img>
+            </div>
+            <img src={aboutMe} alt="" className='about__img'></img>
         </section>
     )
 }
