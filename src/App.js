5d8cb1e0e8c127e5cabf4e54695b7e45db6a6d64
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-01-25 18:32:24 -0500

    add resume v1

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './Components/Header/Header';
 import MainPage from './Pages/MainPage/MainPage';
 import Footer from './Components/Footer/Footer';
+import ResumePage from './Pages/Resume/ResumePage';
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <Header />
-      <MainPage />
+      <Routes>
+        <Route path='/' element={<MainPage />}/>
+        <Route path='/resume' element={<ResumePage />}/>
+      </Routes>
+      
       <Footer />
-    </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Pages/Resume/ResumePage.js
@@ -0,0 +1,14 @@
+import './ResumePage.scss';
+import resumePDF from "../../Assets/resumeimg.png";
+
+function ResumePage() {
+    return (
+        <div className="resume-page">
+            
+            <img className='resume__document' src={resumePDF}/>
+        </div>
+    );
+}
+
+
+export default ResumePage;
