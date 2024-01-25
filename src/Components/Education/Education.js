d167d59050b4f6c0bbdbd0fefdddc61d5d1caa43
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-01-25 17:10:52 -0500

    add work in progress functionality for CE

--- src/Components/Education/Education.js
+++ src/Components/Education/Education.js
@@ -2,31 +2,16 @@ import { useState } from 'react';
 import Modal from '../Modal/Modal.js';
 import "./Education.scss";
 import educationData from "../../Data/education.json";
-
+import ceducationData from "../../Data/continuingEducation.json";
 
 function Education() {
     const [openModalId, setOpenModalId] = useState(null);
-    const [education, setEducation] = useState(educationData)
+    const [education, setEducation] = useState(educationData);
+    const [ceducation, setCeducation] = useState(ceducationData);
     const closeModal = (e) => {
         e.stopPropagation();
         setOpenModalId(null);
     };
-
-    // const CeducationData = [
-    //     {
-    //         id: 1,
-    //         logo: Scrimba,
-    //         name: "Scrimba",
-    //         details: "information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU..."
-    //     },
-    //     {
-    //         id: 2,
-    //         logo: BrainStation,
-    //         name: "123",
-    //         details: "information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU...information about OSU..."
-    //     },
-
-    // ];
 
     return (
         <section className="education">
@@ -49,22 +34,32 @@ function Education() {
                 ))}
             </div>
 
-
             <div>
                 <h2 className="section__title section__title--Ceducation">Continuing Education</h2>
             </div>
-            {/* <div className="ceducation__layout">
-                {CeducationData.map(item => (
-                    <div key={item.id} className="ceducation__item" onClick={() => setOpenModalId(item.id)}>
-                        <img src={item.logo} className="ceducation__logo" />
-                        <div className="ceducation__name">{item.name}</div>
+            <div className="education__layout">
+                {ceducation.map(item => (
+                    <div key={item.id} className="education__item" onClick={() => setOpenModalId(item.id)}>
+                        <img src={require(`../../Assets/education/${item.logo}`)} alt={item.name} className="education__logo" />
+                        <div className="education__name">{item.name}</div>
                         <Modal isOpen={openModalId === item.id} onClose={closeModal}>
-                            <h3>{item.name}</h3>
-                            <p>{item.details}</p>
+                            <h3 className='education__modal-name'>{item.name}</h3>
+                            <p className='education__modal-detail'>{item.details}</p>
+                            <ul>
+                                {item.courses.map((course, index) => (
+                                    <li className='education__modal-item' key={index}>
+                                        {course.title}
+                                        <img className="education__modal-progress"
+                                            src={require(`../../Assets/education/${course.completed ? 'completed.png' : 'wip.png'}`)}
+                                            alt={course.completed ? 'Completed' : 'WIP'}
+                                        />
+                                    </li>
+                                ))}
+                            </ul>
                         </Modal>
                     </div>
                 ))}
-            </div> */}
+            </div>
         </section>
     );
 }
