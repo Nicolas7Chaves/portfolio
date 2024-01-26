85ff3f724d8987bfdf7de83e239869da4de6b99e
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-01-26 16:07:28 -0500

    add resume to page and downloaded react libraries

--- src/Pages/Resume/ResumePage.js
+++ src/Pages/Resume/ResumePage.js
@@ -1,14 +1,40 @@
+import { useState, useRef, useEffect } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+import resumePDF from '../../Assets/ncResume.pdf';
+//Do not remove the imports below they disable the annotation layer and text layer.
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
+import 'react-pdf/dist/esm/Page/TextLayer.css';
+import download from "../../Assets/download.svg"
 import './ResumePage.scss';
-import resumePDF from "../../Assets/resumeimg.png";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 function ResumePage() {
+    const [pageNumber] = useState(1);
+    const [pageWidth, setPageWidth] = useState(window.innerWidth);
+    const containerRef = useRef();
+
+    useEffect(() => {
+        const handleResize = () => {
+            setPageWidth(containerRef.current.clientWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+        handleResize();
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
-        <div className="resume">
-            
-            <img className='resume__document' src={resumePDF}/>
+        <div className='resume-layout'>
+            <div className="resume" ref={containerRef}>
+                <a href={resumePDF} download className="download-button"><img className='resume__download' src={download} /></a>
+                <Document file={resumePDF}>
+                    <Page pageNumber={pageNumber} width={pageWidth} />
+                </Document>
+            </div>
         </div>
     );
 }
-
 
 export default ResumePage;
