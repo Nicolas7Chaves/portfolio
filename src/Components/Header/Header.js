c557d13e9772f3980743c2fd119b8826e4203b4d
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-01-29 16:20:48 -0500

    add NC Logo to header

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,6 +1,9 @@
 import { useRef } from 'react';
 import './Header.scss';
 import { FaBars, FaTimes } from "react-icons/fa";
+import ncLogo from "../../Assets/nc.png";
+import { Link } from 'react-router-dom';
+
 
 function Header() {
     const navRef = useRef();
@@ -13,17 +16,19 @@ function Header() {
 
     return (
         <header className="header">
-            <div className="header__logo">
-                <img src="" alt="" />
+            <div className="header__logo-container">
+                <Link to="/">
+                    <img className='header__logo' src={ncLogo} alt="NC LOGO" />
+                </Link>
             </div>
             <nav className="header__nav " ref={navRef}>
-                    <a className="header__item" href="/#home" onClick={closeNavbar}>Home</a>
-                    <a className="header__item" href="/#about" onClick={closeNavbar}>About</a>
-                    <a className="header__item" href="/#projects" onClick={closeNavbar}>Projects</a>
-                    <a className="header__item" href="/resume" onClick={closeNavbar}>Resume</a>
-                    <button className='header__btn header__close-btn' onClick={showNavbar}>
-                        <FaTimes />
-                    </button>
+                <a className="header__item" href="/#home" onClick={closeNavbar}>Home</a>
+                <a className="header__item" href="/#about" onClick={closeNavbar}>About</a>
+                <a className="header__item" href="/#projects" onClick={closeNavbar}>Projects</a>
+                <a className="header__item" href="/resume" onClick={closeNavbar}>Resume</a>
+                <button className='header__btn header__close-btn' onClick={showNavbar}>
+                    <FaTimes />
+                </button>
             </nav>
             <button className='header__btn' onClick={showNavbar}>
                 <FaBars />
