761aa591da4632dfbd66334b4722ffe2d3fc5611
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-05-24 14:53:18 -0400

    add changes to courses and bio

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,12 +10,12 @@ function About() {
             <div className='about__layout'>
                 <h2 className="section__title section__title--about">Who I am</h2>
                 <TypeAnimation
-                    sequence={['Software Engineer', 600,'Project Manager', 600,'Analyst', 600, 'Golfer', 600, 'Crossfitter', 600]}
+                    sequence={['Product Manager', 600,'Project Manager', 600,'Analyst', 600, 'Golfer', 600]}
                     repeat={Infinity}
                     className="section__subtitle--about"
                 />
                 <div className="about__body">
-                    I'm a recent graduate from BrainStation, where I cultivated a passion for Software Development with a strong background in Real Estate Analysis. I'm eager to continue my journey in Software Engineering, applying my creativity and problem-solving skills to develop innovative solutions.
+                I'm transitioning to Product Management, I leverage my background in Real Estate Finance, Project Management, and full-stack development. Expertise in strategic planning, stakeholder management, and technical proficiency enables me to effectively connect development teams with business goals.
                 </div>
             </div>
             <img src={aboutMe} alt="" className='about__img'></img>
